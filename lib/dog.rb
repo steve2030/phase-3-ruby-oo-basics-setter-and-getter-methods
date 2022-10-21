35c7c75c867bf83353d549809360259d6f22ec0f
steve <stevemighty2030@gmail.com>
2022-10-21 16:37:11 +0300

    passed the labs tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+  # name getter
+  def name=(name)
+    @name=name
+  end
+  # breed getter
+  def breed=(breed)
+    @breed=breed
+  end
 
+  # name setter
+  def name
+    @name
+  end
+  # breed setter
+  def breed
+    @breed
+  end
 end
+
+
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+  # name getter
+  def name=(name)
+    @name=name
+  end
+  # job getter
+  def job=(job)
+    @job=job
+  end
+# name setter
+  def name
+    @name
+  end
+  # job setter
+  def job
+    @job
+  end
+
 
 end
